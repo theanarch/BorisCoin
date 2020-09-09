719866e48a63e1595793442aaa4c0bcf926c5c6d
Boris Torbica <tesla02_10@hotmail.com>
2020-09-09 20:39:07 +0200

    Running first account

--- contracts/FirstEth.sol
+++ contracts/FirstEth.sol
@@ -3,5 +3,22 @@ pragma solidity ^0.5.0;
 contract FirstEth {
     uint public taskCount = 0;
 
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks; // uint = key, Task = value
+
+    constructor() public {
+        createTask("My first ETH task");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+
+    }
 
 }
